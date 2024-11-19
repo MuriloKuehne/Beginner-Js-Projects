35e0acb1ae1955f18843a684add964d676b56add
Murilo Kuehne <murkuehne@gmail.com>
2024-11-18 23:04:00 -0300

    first commit

---
+++ Heart Trail Animation/index.js
@@ -0,0 +1,17 @@
+const bodyElem = document.querySelector("body");
+
+bodyElem.addEventListener("mousemove", (event) => {
+  const xPos = event.offsetX;
+  const yPos = event.offsetY;
+  const spanElem = document.createElement("span");
+  const size = Math.random() * 100;
+  spanElem.style.left = xPos + "px";
+  spanElem.style.top = yPos + "px";
+  spanElem.style.width = size + "px";
+  spanElem.style.height = size + "px";
+
+  bodyElem.appendChild(spanElem);
+  setTimeout(() => {
+    spanElem.remove();
+  }, 3000);
+});
